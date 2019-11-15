01dd61088fdfff5989a953e40ed196a1140cae7b
Lea <lea.levy406@gmail.com>
2019-11-15 02:39:54 +0100

    style & comm unworkin

--- fb_lite/src/index.js
+++ fb_lite/src/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import lion from './stuff/lion.jpg';
+import spidey from './stuff/spidey.jpg';
 import sg from './stuff/sg.jpg';
 import steve from './stuff/steve.jpg';
+import { genericTypeAnnotation } from '@babel/types';
 
-function buttonStyle(){
 
+function buttonStyle(props) {
+    return (
+    <div className="style">
+        <button className={props.state.button ? "cliked": "notCliked"} onClick={props.handleClick}>Changer Style</button>  
+    </div>
+    )
 }
-
 
 class Profile extends React.Component {
   constructor(props) {
@@ -18,55 +23,85 @@ class Profile extends React.Component {
             nom: 'Rogers',
             prenom: 'Steve',
             ddn: '2 janvier 1987',
-            pp: steve
+            pp: steve,
+            background: "grey",
+            post: "A la recherche de mon bouclier, contactez-moi si vous le trouvez."
           },
           {
             nom: 'Parker',
-            prenom: 'Billy',
+            prenom: 'Peter',
             ddn: '7 juillet 1965',
-            pp: lion
+            pp: spidey,
+            background: "grey",
+            post: "Spider-verse 2 : coming soon"
           },
           {
             nom: 'Kara',
             prenom: 'Danvers',
             ddn: '12 septembre 1977',
-            pp: sg
+            pp: sg,
+            background: "grey",
+            post: "Not related to Carol Danvers"
           }
         ],
         idUser: 0,
+        button: true
     };
   }
 
   handleClick(i) {
-    this.setState({idUser : i})
+    this.setState(
+        {idUser: i,
+        button: !this.state.button}
+        );
   }
 
   render() {
     return (
       <div className="profile">
+        <div className="change">
+            <button onClick={() => this.handleClick(0)}>{this.state.profiles[0].prenom}</button>
+            <button onClick={() => this.handleClick(1)}>{this.state.profiles[1].prenom}</button>
+            <button onClick={() => this.handleClick(2)}>{this.state.profiles[2].prenom}</button>
+        </div>
         <div className="infos">
             <img src={this.state.profiles[this.state.idUser].pp}></img>
             <article>
-                <div className="first_name">
-                    <p style={{color : this.state.color}}>{this.state.profiles[this.state.idUser].prenom}</p>
+                <div className="prenom">
+                    <p>{this.state.profiles[this.state.idUser].prenom}</p>
                 </div>
-                <div className="last_name">
-                    <p style={{color : this.state.color}}>{this.state.profiles[this.state.idUser].nom}</p>
+                <div className="nom">
+                    <p>{this.state.profiles[this.state.idUser].nom}</p>
                 </div>
-                <div className="date">
-                    <p style={{color : this.state.color}}>{this.state.profiles[this.state.idUser].ddn}</p>
+                <div className="ddn">
+                    <p>{this.state.profiles[this.state.idUser].ddn}</p>
                 </div>
             </article>
-
-            <button>Changer Style</button>
-
         </div>
 
-    
+        <div className="publi">
+            <Publication
+                publi={this.state.profiles[this.state.idUser].post}
+            />
+        </div>
+        
       </div>
     );
   }
 }
+
+class Publication extends React.Component{
+    render() {
+        return(
+            <div>
+                <article>
+                    <p>{this.props.post}</p>
+                </article>
+            </div>
+        );
+    }
+}
+
 
 ReactDOM.render(
     <Profile/>,
